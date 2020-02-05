75377162285fc912a8ce873a3963afc6d0ddfbfb
MANSON Tomy <tomy.manson@thalesgroup.com>
2020-02-05 18:30:56 +0100

    Ajout angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { CoreModule } from './core/core.module';
 import { APP_CONFIG, APP_CONSTANTS } from './app.config';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { APP_CONFIG, APP_CONSTANTS } from './app.config';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [
     { provide: APP_CONFIG, useValue: APP_CONSTANTS },
